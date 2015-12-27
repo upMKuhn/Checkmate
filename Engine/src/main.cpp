9632e8163c6d69ba588d9e0af881b6f97dc4d340
up687791 <up687791@myport.ac.uk>
2015-12-27 15:51:17 +0100

    ShowBoard takes fen

--- Engine/src/main.cpp
+++ Engine/src/main.cpp
@@ -54,20 +54,17 @@ void performanceTest()
 }
 
 
-void ShowBoard(Color c)
+void ShowBoard(Color c, string fen)
 {
 	using namespace Checkmate; using namespace DataStructs; using namespace std;
+	Represenation& board = *(new Represenation(fen));
 	string token, cmd;
-	string startFen =  c == WHITE ?"rnbqkbnr/P7/8/5PpP/8/4p3/4pP1P/RNBQK2R w KQkq G5 0 1" 
-							: "rnbqkbnr/P7/8/5PpP/4Q3/4p3/4pP1P/RNBQK2R b KQkq G5 0 1";
-	Represenation& board = *(new Represenation(startFen));
 
 	MoveList* mvlist = new MoveList();
 	MoveGen& movgen = *(new MoveGen(board));
 
 	mvlist = movgen.generate_all(c).move_list;
 	
-	std::cout << "to move: " << (c == WHITE ? "WHITE" : "BLACK") << std::endl;
 
 	while(mvlist->index > 0)
 	{
@@ -75,6 +72,7 @@ void ShowBoard(Color c)
 		std::cout << board.to_string() << std::endl;
 		mvlist = mvlist->node;
 		board.undoMove();
+		std::cout << "to move: " << (c == WHITE ? "WHITE" : "BLACK") << std::endl;
 		getline(cin, cmd);
 	}
 
@@ -87,10 +85,10 @@ int main(int argc, char* argv[]) {
 	string token, cmd;
 	Represenation board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
 	
-	//performanceTest();
+	performanceTest();
 	
-	ShowBoard(BLACK);
-	ShowBoard(WHITE);
+	ShowBoard(BLACK, "rnbqkbnr/P7/8/5PpP/4Q3/4p3/4pP1P/RNBQK2R b KQkq G5 0 1");
+	ShowBoard(WHITE, "rnbqkbnr/P7/8/5PpP/8/4p3/4pP1P/RNBQK2R w KQkq G5 0 1");
 	
 	std::cout << board.to_string() << std::endl;
 	system("PAUSE");
